bb086a8ad034622c0f5d83dbd0385c5eeca848e8
cyubimanyo <andhiny.aye@gmail.com>
2023-12-01 15:36:23 +0700

    login feature with 4 test cases

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login with 4 Test Cases', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+
+  it('Login with Valid Data', () => {
+    cy.readFile('cypress/fixtures/validLogin.json').then((account) => {
+      cy.login(account.username, account.password)
+      cy.get('.oxd-topbar-header').should('be.visible')
+    })
+  })
+
+  it('Login with Invalid Data', () => {
+    cy.readFile('cypress/fixtures/invalidLogin.json').then((account) => {
+      cy.login(account.username, account.password)
+      cy.get('.oxd-alert').should('be.visible')
+    })
+  })
+
+  it('Login with Blank Data', () => {
+    cy.get('.oxd-button').should('be.visible').click()
+    cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('be.visible')
+  })
+
+  it.only('Forgot Password', () => {
+    cy.get('.orangehrm-login-forgot > .oxd-text').should('be.visible').click()
+    cy.get('.oxd-input').should('be.visible').type('anything')
+    cy.get('.oxd-button--secondary').should('be.visible').click()
+    cy.get('.oxd-text--h6').contains('Reset Password link sent successfully')
   })
 })
